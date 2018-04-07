e8a27b2ef92f2695b7dae487f175cd5366af363b
Amuuo <30686676+Amuuo@users.noreply.github.com>
2018-04-06 20:26:41 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,42 +1,127 @@
-//Major Project 1
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include<sys/wait.h>
 
-int main(int argc char* argv[])
+int main(int argc, char **argv)
 {
-  int i;
-  int clength;
-  int currentArg;
-  char* arg;
-  pid_t pid;
-  
-  //cycle through command-line args
-  for(i = 0; i < argc; ++i)
-  { 
-    clength = strlen(argv[i]);
-    //if ';' follows command, fork the process    
-    if(argv[i][clength] == ';')
-    {
-      pid = fork();
-      if(pid == -1)
-      {
-        printf("%s", "Error: pid = -1");
-        exit(1);
-      }
-      printf("%s %d", "My pid = ", getpid());
-      arg = argv[argc]
-      exec(arg);
-    }
-    //if ';' does not follow command, run new image
-    else
-    {
-      char* path = "/bin/";
-      strcat(path, argv[argc]);
-      execv(path, argv[]);
-      printf("Something went wrong with execv() command");
-    }
-  }
-  return 0;
+	pid_t pid;
+	int status;
+	
+	int i, x, temp, counter, arg;
+	long size;
+	char input[512];
+	char *buffer, *cwd;
+	char *command, **cmds, *argument, **args;
+	char *endCmnd;
+	
+	if(argc == 1)
+	{
+		while(1)
+		{
+			i       = 0;
+			x       = 0;
+			temp    = 0;
+			counter = 0;
+			printf("> ");
+
+			//retrieve user-entered command
+			fgets(input, 512, stdin);
+			if(input[strlen(input) - 2] != ';')
+			{
+				counter++;
+			}	
+			for(x = 0; x < strlen(input); x++)
+			{
+				if(input[x] == ';')
+				{
+					counter++;
+				}
+			}
+			cmds = malloc(counter*sizeof(char*));
+			command = strtok_r(input, ";", &endCmnd);
+			while(command != NULL)
+			{
+				pid = fork();
+				char *endArg;
+				cmds[i] = malloc(strlen(command)*sizeof(char));
+				strcpy(cmds[i], command);
+				i++;
+				arg = 1;
+				for(x = 0; x < strlen(cmds[i]) + 1; x++)
+				{
+					if(cmds[i][x] == ' ')
+					{
+						if(x == 0)
+						{
+						}
+						else
+						{
+							arg++;
+						}
+					}
+				}
+					args = malloc(arg*sizeof(char*));
+					argument = strtok_r(command, " ", &endArg);
+					while(argument != NULL)
+					{
+						args[temp] = malloc(strlen(argument)*sizeof(char));
+						strcpy(args[temp], argument);
+						temp++;
+						argument = strtok_r(NULL, " ", &endArg);
+					}
+				/*if(pid == 0)
+				{
+					for(i = 0; i < counter; i++)
+					{
+						if(strcmp(cmds[i], "pwd") == 0)
+						{
+							cwd = getcwd(buffer, 0);
+							printf("%s", cwd);
+						}
+						else if(strcmp(cmds[i], "cd") == 0)
+						{
+							chdir(getenv("HOME"));
+						}
+						else if(strcmp(cmds[i], "cd") > 0)
+						{
+							chdir(args[1]);
+						}
+						else
+						{
+							execvp(args[0], args);
+						}
+					}
+				}
+				else if(pid > 0)
+				{
+					if(strcmp(cmds[i], "exit") == 0)
+					{
+						wait(0);
+						exit(0);
+					}
+				}
+				else
+				{
+					perror("Fork");
+					exit(1);
+				}*/
+				command = strtok_r(NULL, ";", &endCmnd);
+			}
+		}
+	}
+	else if(argc > 1)
+	{
+		// Read from batch file
+	}
+	else
+	{
+		printf("Invalid, too many arguments");
+		exit(0);
+	}
+	
+	return 0;
 }
